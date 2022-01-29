227a00c23e2d5084da2719f641e550a5ec3e5d50
jcromanu <jcromanurgell@gmail.com>
2022-01-28 23:53:42 -0600

    fix(hardcoding) fixed hardcoding on main funcs

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.17
 require github.com/stretchr/testify v1.7.0
 
 require (
+	github.com/caarlos0/env/v6 v6.9.1 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-kit/kit v0.12.0 // indirect
 	github.com/go-kit/log v0.2.0 // indirect
--- http_service/cmd/main.go
+++ http_service/cmd/main.go
@@ -8,6 +8,7 @@ import (
 	"os/signal"
 	"syscall"
 
+	"github.com/caarlos0/env/v6"
 	"github.com/go-kit/kit/endpoint"
 	"github.com/go-kit/log"
 	"github.com/go-kit/log/level"
@@ -18,30 +19,14 @@ import (
 
 func main() {
 
-	var (
-		logger      log.Logger
-		userGRPC    *grpc.ClientConn
-		grpcErr     error
-		middlewares = []endpoint.Middleware{}
-		opts        []grpc.DialOption
-	)
-	opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
-	httpAddr := flag.String("http.addr", ":8081", "HTTP listen address")
-
-	logger = log.NewLogfmtLogger(os.Stderr)
-	logger = log.With(logger, "ts", log.DefaultTimestampUTC)
-	logger = log.With(logger, "caller", log.DefaultCaller)
-	addr := "localhost:8080"
-	userGRPC, grpcErr = grpc.Dial(addr, opts...)
-	if grpcErr != nil {
-		level.Error(logger).Log("gRPC", grpcErr)
-		os.Exit(-1)
+	var logger log.Logger
+	{
+		logger = log.NewLogfmtLogger(os.Stderr)
+		logger = log.With(logger, "ts", log.DefaultTimestampUTC)
+		logger = log.With(logger, "caller", log.DefaultCaller)
 	}
-
-	repo := httpuserservice.NewHttpRespository(userGRPC, logger)
-	srv := httpuserservice.NewHttpService(repo, logger)
-	endpoints := httpuserservice.MakeEndpoints(srv, logger, middlewares)
-	httpserver := httpuserservice.NewHTTPServer(endpoints, logger)
+	middlewares := []endpoint.Middleware{}
+	cfg := serverConfig{}
 
 	errs := make(chan error)
 	go func() {
@@ -50,10 +35,36 @@ func main() {
 		errs <- fmt.Errorf("%s", <-c)
 	}()
 
+	level.Info(logger).Log("msg", "http service started ")
+	defer level.Info(logger).Log("msg", "htpp service stoped")
+
+	if err := env.Parse(&cfg); err != nil {
+		level.Error(logger).Log("Error retrieviing env variables using default ones ")
+	}
+	grpcAddr := fmt.Sprintf("%s:%d", cfg.Hostname, cfg.GrpcPort)
+	userGRPC, grpcErr := grpc.Dial(grpcAddr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if grpcErr != nil {
+		level.Error(logger).Log("gRPC", grpcErr)
+		os.Exit(-1)
+	}
+	httpPort := fmt.Sprintf(":%d", cfg.HttpPort)
+	httpAddr := flag.String("http.addr", httpPort, "HTTP listen address")
+
+	repo := httpuserservice.NewHttpRespository(userGRPC, logger)
+	srv := httpuserservice.NewHttpService(repo, logger)
+	endpoints := httpuserservice.MakeEndpoints(srv, logger, middlewares)
+	httpserver := httpuserservice.NewHTTPServer(endpoints, logger)
+
 	go func() {
 		logger.Log("transport", "HTTP", "addr", *httpAddr)
 		errs <- http.ListenAndServe(*httpAddr, httpserver)
 	}()
 
-	logger.Log("exit", <-errs)
+	logger.Log("http client server exit", <-errs)
+}
+
+type serverConfig struct {
+	Hostname string `env:"HOSTNAME" envDefault:"localhost"`
+	HttpPort int    `env:"HTTP_SERVER_PORT" envDefault:"8081"`
+	GrpcPort int    `env:"GRPC_SERVER_PORT" envDefault:"8080"`
 }
