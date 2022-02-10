1631ca478dbb478edb946492fd371e334281fb3f
jcromanu <jcromanurgell@gmail.com>
2022-02-10 13:12:24 -0600

    mix addition of mongo refactor of constant

--- user_service/pkg/user_service/repository.go
+++ user_service/pkg/user_service/repository.go
@@ -12,11 +12,11 @@ import (
 )
 
 const (
-	create_user_sql = `INSERT INTO USER(username,age,pwd_hash,additional_information,parent) 
+	createUserSQL = `INSERT INTO USER(username,age,pwd_hash,additional_information,parent) 
 	VALUES(?,?,?,?,?)`
-	get_user_sql    = `SELECT u.username , u.pwd_hash , u.additional_information , u.age , u.parent FROM USER u  WHERE u.id = ? `
-	update_user_sql = "UPDATE USER set username = ? , age = ? , pwd_hash = ? , additional_information = ? , parent = ? where id = ?  "
-	delete_user_sql = `DELETE FROM USER WHERE id = ? `
+	getUserSQL    = `SELECT u.username , u.pwd_hash , u.additional_information , u.age , u.parent FROM USER u  WHERE u.id = ? `
+	updateUserSQL = "UPDATE USER set username = ? , age = ? , pwd_hash = ? , additional_information = ? , parent = ? where id = ?  "
+	deleteUserSQL = `DELETE FROM USER WHERE id = ? `
 )
 
 type userRepository struct {
@@ -33,7 +33,7 @@ func NewUserRepository(db *sql.DB, log log.Logger) *userRepository {
 
 func (r *userRepository) CreateUser(ctx context.Context, usr entities.User) (int32, error) {
 	var res sql.Result
-	res, err := r.db.ExecContext(ctx, create_user_sql, usr.Name, usr.Age, usr.Pwd_hash, usr.Additional_information, strings.Join(usr.Parent, ","))
+	res, err := r.db.ExecContext(ctx, createUserSQL, usr.Name, usr.Age, usr.Pwd_hash, usr.Additional_information, strings.Join(usr.Parent, ","))
 	if err != nil {
 		level.Error(r.log).Log("Error creating user" + err.Error())
 		return 0, errors.NewInternalError()
@@ -45,7 +45,7 @@ func (r *userRepository) CreateUser(ctx context.Context, usr entities.User) (int
 func (r *userRepository) GetUser(ctx context.Context, id int32) (entities.User, error) {
 	var parent *string
 	usr := entities.User{}
-	err := r.db.QueryRow(get_user_sql, id).Scan(&usr.Name, &usr.Pwd_hash, &usr.Additional_information, &usr.Age, &parent)
+	err := r.db.QueryRow(getUserSQL, id).Scan(&usr.Name, &usr.Pwd_hash, &usr.Additional_information, &usr.Age, &parent)
 	if err == sql.ErrNoRows {
 		level.Error(r.log).Log("Error retrieving user" + err.Error())
 		return entities.User{}, errors.NewUserNotFoundError()
@@ -61,12 +61,12 @@ func (r *userRepository) GetUser(ctx context.Context, id int32) (entities.User, 
 
 func (r *userRepository) UpdateUser(ctx context.Context, usr entities.User) error {
 
-	if err := r.db.QueryRow(get_user_sql, usr.Id).Scan(); err == sql.ErrNoRows {
+	if err := r.db.QueryRow(getUserSQL, usr.Id).Scan(); err == sql.ErrNoRows {
 		level.Error(r.log).Log("Error retrieving user ")
 		return errors.NewUserNotFoundError()
 	}
 
-	_, err := r.db.ExecContext(ctx, update_user_sql, usr.Name, usr.Age, usr.Pwd_hash, usr.Additional_information, strings.Join(usr.Parent, ","), usr.Id)
+	_, err := r.db.ExecContext(ctx, updateUserSQL, usr.Name, usr.Age, usr.Pwd_hash, usr.Additional_information, strings.Join(usr.Parent, ","), usr.Id)
 	if err != nil {
 		level.Error(r.log).Log("Error updating user " + err.Error())
 		return errors.NewInternalError()
@@ -75,11 +75,11 @@ func (r *userRepository) UpdateUser(ctx context.Context, usr entities.User) erro
 }
 
 func (r *userRepository) DeleteUser(ctx context.Context, id int32) error {
-	if err := r.db.QueryRow(get_user_sql, id).Scan(); err == sql.ErrNoRows {
+	if err := r.db.QueryRow(getUserSQL, id).Scan(); err == sql.ErrNoRows {
 		level.Error(r.log).Log("Error deleting user ")
 		return errors.NewUserNotFoundError()
 	}
-	_, err := r.db.ExecContext(ctx, delete_user_sql, id)
+	_, err := r.db.ExecContext(ctx, deleteUserSQL, id)
 	if err != nil {
 		level.Error(r.log).Log("Error deleting user " + err.Error())
 		return errors.NewInternalError()
